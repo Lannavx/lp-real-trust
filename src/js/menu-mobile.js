39eea377db6b54bef2375a9f04846cf694195118
Lannavx <lanna_148@hotmail.com>
2024-11-02 15:41:30 -0300

    Refatorando projeto para mobile first e aplicando CSS modular

---
+++ src/js/menu-mobile.js
@@ -0,0 +1,10 @@
+const btnMobile = document.querySelector("#btn-mobile");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.preventDefault();
+  const nav = document.querySelector("#menu");
+  nav.classList.toggle("active");
+}
+
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
